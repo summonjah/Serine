09b286dae68d4183cbef2316f2734335f6ef6a48
std-fs <155996914+std-fs@users.noreply.github.com>
2024-02-16 18:26:17 +0800

    Update PrintLoadstring.lua

--- Serine Luau Script's/PrintLoadstring.lua
+++ Serine Luau Script's/PrintLoadstring.lua
@@ -1,1 +1,6 @@
-print("Serine Test..")
+
+local skdisdkid = require("skid")
+-- he new that i am skid :GRGRR:
+local ciloveskids = skdisdkid:new()
+--cracked serine niga
+cdiloveskids:info("Serine Test..")
